21682f2b2b3c1e4d13ce0d94738464676f4866e3
Maximilian Maroe <drakfyre@gmail.com>
2014-12-08 14:37:09 -0800

    Music and timer

---
+++ Assets/Scripts/TextTimer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextTimer : MonoBehaviour
+{
+	private Text text;
+	private float time = 0.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		time += Time.deltaTime;
+		text.text = time.ToString("f2");
+	}
+}
