1948e63f5240cce74ec1b8d2ccb00c9a4516bd59
Maximilian Maroe <drakfyre@gmail.com>
2014-12-08 14:48:40 -0800

    Basic game over

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance;
+
+	public GameObject gameOverDisplay;
+
+	private bool gameOver = false;
+	
+	void Awake()
+	{
+		Instance = this;
+	}
+
+	public static void GameOver()
+	{
+		if(Instance.gameOver == false)
+		{
+			Instance.gameOver = true;
+			Instance.gameOverDisplay.SetActive(true);
+		}
+	}
+}
