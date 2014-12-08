1fd9e359d18c6b21269f475feae7ad1377739f36
Maximilian Maroe <drakfyre@gmail.com>
2014-12-08 13:43:33 -0800

    Tweaks

--- Assets/FollowMouse.cs
+++ Assets/FollowMouse.cs
@@ -6,7 +6,7 @@ public class FollowMouse : MonoBehaviour
 	public float distanceFromCamera = 10.0f;
 	
 	// Update is called once per frame
-	void LateUpdate ()
+	void Update ()
 	{
 		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward*distanceFromCamera);
 	}
