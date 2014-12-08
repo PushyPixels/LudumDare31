e29a3957c9c3a229d6ec5d7510ea3fe88589ff63
Maximilian Maroe <drakfyre@gmail.com>
2014-12-08 14:14:04 -0800

    Basic level logic.  (Real basic, but it's a real basic game)

---
+++ Assets/Scripts/SpawnRandomly.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnRandomly : MonoBehaviour
+{
+	public GameObject[] objectsToSpawn;
+	public float spawnRadius = 10.0f;
+	public float minSpawnDelay = 1.0f;
+	public float maxSpawnDelay = 2.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Invoke("Spawn",Random.Range(minSpawnDelay,maxSpawnDelay));
+	}
+
+	void Spawn ()
+	{
+		Instantiate(objectsToSpawn[Random.Range(0,objectsToSpawn.Length)],transform.position + (Vector3)Random.insideUnitCircle*spawnRadius, Quaternion.LookRotation(Random.insideUnitCircle));
+		Invoke("Spawn",Random.Range(minSpawnDelay,maxSpawnDelay));
+	}
+}
