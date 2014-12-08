397258cd063cf1dd121155b014af80ecdd0b983d
Maximilian Maroe <drakfyre@gmail.com>
2014-12-08 13:16:53 -0800

    Basic mouse movement and trail renderer

---
+++ Assets/FollowMouse.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowMouse : MonoBehaviour
+{
+	public float distanceFromCamera = 10.0f;
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward*distanceFromCamera);
+	}
+}
