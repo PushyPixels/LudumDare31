bdd8391a3664f6f5d23af04a7f6d5fa35789eaf9
Maximilian Maroe <drakfyre@gmail.com>
2014-12-08 13:30:43 -0800

    Object trail!

---
+++ Assets/ObjectTrail.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectTrail : MonoBehaviour
+{
+	public GameObject trailObject;
+	public float newObjectDistance = 1.0f;
+	public float sizeRatio = 1.0f;
+
+	private Vector3 currentObjectStartPosition;
+	private GameObject currentObject;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if(currentObject == null)
+		{
+			currentObject = Instantiate(trailObject,transform.position,Quaternion.identity) as GameObject;
+			currentObjectStartPosition = transform.position;
+		}
+		else
+		{
+			Vector3 toTransformVector = transform.position - currentObjectStartPosition;
+
+			currentObject.transform.rotation = Quaternion.LookRotation(toTransformVector);
+
+			Vector3 newScale = currentObject.transform.localScale;
+			float distance = toTransformVector.magnitude;
+			newScale.z = distance*sizeRatio;
+			currentObject.transform.localScale = newScale;
+
+
+			if(distance >= newObjectDistance)
+			{
+				currentObject = Instantiate(trailObject,transform.position,Quaternion.identity) as GameObject;
+				currentObjectStartPosition = transform.position;
+			}
+		}
+	}
+}
